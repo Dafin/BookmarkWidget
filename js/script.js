b3f7eb727e5195e110cdba6a9596190c8155d361
Dafin <dafin@users.noreply.github.com>
2017-06-13 16:40:59 +0100

    - Moved Javascript into seperate file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+  function add() {
+    // grab
+    var ulArray = document.getElementsByClassName("TheList");
+    console.log(ulArray);
+
+    var firstUl = ulArray[0];
+    console.log(firstUl)
+
+
+    // add
+    var newItem = document.createElement ('li');
+
+    newItem.innerHTML = randomString;
+    firstUl.appendChild(newItem);
+  }
