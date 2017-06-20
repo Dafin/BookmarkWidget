058fad6fc28b592e36078b9e300a08d7bfd4cc4d
Dafin <dafin@users.noreply.github.com>
2017-06-20 02:48:10 +0100

    - Started implementing jquery for DOM traversal - Input box now clears after succesful addition to the list

--- js/script.js
+++ js/script.js
@@ -9,20 +9,27 @@
 
     // add
     
-    var thisGrabbed = document.getElementById("inputHere"); 
+    var thisGrabbed = $('#inputHere'); 
     var newItem = document.createElement ('li');
 
-    var grabbed = thisGrabbed.value;
+    var grabbed = thisGrabbed.val()
     var tNode = document.createTextNode(grabbed);  
 
 
     console.log("it was: " + grabbed)
 
-
+    // check for blank entry
     if (grabbed  == "") {
-        alert("U wot m8")
+        alert("U wot m8");
+        
+    // *Not functional - dupe check     
+    //} else if (grabbed === tNode.value){
+    //    alert("dupe");
+    //    inputHere.value = '';
+
     } else {
         firstUl.appendChild(newItem);
         newItem.appendChild(tNode);
+        inputHere.value = '';
     }
   }
